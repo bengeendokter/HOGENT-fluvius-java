02a748657512a657fee1af1a85433feb0ac188a9
Mert Sahin <mert.sahin@student.hogent.be>
2022-02-23 13:41:28 +0100

    commit testklasse

--- src/testen/DomeinTest.java
+++ src/testen/DomeinTest.java
@@ -1,5 +1,58 @@
 package testen;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class DomeinTest {
-	//Test van TestKlasse
+	//Bestaande gebruikers in de databank
+	//JanJansens, 123456789
+	//JansensJan, 987654321
+	
+	//vraag aan de factory een entityManager
+    EntityManager entityManager =  JPAUtil.getEntityManagerFactory().createEntityManager();
+    
+    ////start een transactie
+    entityManager.getTransaction().begin();
+
+    Docent docent  = entityManager.find(Docent.class, 2L);
+		
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void controleerGebruiker(int pins, int total) {
+		
+	}
+	
+	
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void meldAan_bestaandGebruikerJuisteGegevensGeldigPoging_Login(int pins, int total) {
+		Assertions.assertEquals(true, true);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void meldAan_bestaandGebruikerFouteGegevensGeldigPoging_Exception(int pins, int total) {
+		Assertions.assertEquals(true, true);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void meldAan_onbestaandGebruikerGeldigPoging_Exception(int pins, int total) {
+		Assertions.assertEquals(true, true);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void meldAan_bestaandGebruikerFouteGegevensOngeldigPoging_Exception(int pins, int total) {
+		Assertions.assertEquals(true, true);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"0, 0", "1,20"})
+	public void meldAan_onbestaandGebruikerOngeldigPoging_Exception(int pins, int total) {
+		Assertions.assertEquals(true, true);
+	}
+	
+	
 }
