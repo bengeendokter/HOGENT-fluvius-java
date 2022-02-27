29c68f9e643fb498901a1c2a5a78938fc2b6a8fe
Ben <79012763+benartshogent@users.noreply.github.com>
2022-02-27 11:40:35 +0100

    DC generate VVP to code

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -1,11 +1,17 @@
 package domein;
 
+import java.util.List;
+
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import repository.GenericDaoJpa;
 
 public class DomeinController
 {
 	private Gebruiker aangemeldeGebruiker;
 	
+	private Fluvius fluvius;
+
 	public DomeinController(Gebruiker aangemeldeGebruiker)
 	{
 		this.aangemeldeGebruiker = aangemeldeGebruiker;
@@ -19,6 +25,46 @@ public class DomeinController
 	public void sluitPersistentie()
 	{
 		GenericDaoJpa.closePersistency();
+	}
+
+	public void voegCategorieToe(Categorie categorie)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void verwijderCategorie(Categorie categorie)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public ObservableList<Categorie> geefCategorien()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void voegCategorieObserverToe(ListChangeListener<Categorie> listener)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public ObservableList<MvoDoelstelling> geefDoelstellingen()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void wijzigCategorieNaam(Categorie categorie, String naam)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void wijzigCategorieRollen(Categorie categorie, List<Rol> rollen)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void wijzigCategorieDoelstellingen(Categorie categorie, List<MvoDoelstelling> doelstellingen)
+	{
+		throw new UnsupportedOperationException();
 	}
 	
 }
