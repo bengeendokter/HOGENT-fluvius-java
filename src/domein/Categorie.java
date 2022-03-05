3f2b9a75024e7289ee2f55b27f04175e11d840ce
CasDS98 <79106753+CasDS98@users.noreply.github.com>
2022-03-05 13:15:57 +0100

    Categorie interface implementatie

--- src/domein/Categorie.java
+++ src/domein/Categorie.java
@@ -1,5 +1,9 @@
 package domein;
+import java.util.List;
 
 public interface Categorie {
-
+	public int getCategorieID();
+	public String getNaam();
+	public String getIcon();
+	public List<SdGoal> getSdGoals();
 }
