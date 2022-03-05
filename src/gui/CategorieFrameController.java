8b953928d9beaa72cba8529f83e567143e4041df
Mert Sahin <mert.sahin@student.hogent.be>
2022-03-05 17:39:00 +0100

    Iconen beheren zijn specifiek

--- src/gui/CategorieFrameController.java
+++ src/gui/CategorieFrameController.java
@@ -149,13 +149,14 @@ public class CategorieFrameController extends Pane
 			catOpslaan.setVisible(false);
 			catAnnuleer.setVisible(false);
 			
+			//iconen passen bij scherm van beheren
 			tabPane.getTabs().forEach(e -> {
 	            if (e.getText().equals("MVO doelstelling beheren"))
-	                e.setGraphic(new ImageView(new Image("file:src/images/people.png", 25, 25, true, true)));
+	                e.setGraphic(new ImageView(new Image("file:src/images/doelstelling.png", 25, 25, true, true)));
 	            else if (e.getText().equals("Categorie beheren"))
-	                e.setGraphic(new ImageView(new Image("file:src/images/partnership.png", 25, 25, true, true)));
+	                e.setGraphic(new ImageView(new Image("file:src/images/category.png", 25, 25, true, true)));
 	            else if (e.getText().equals("Datasource beheren"))
-	                e.setGraphic(new ImageView(new Image("file:src/images/prosperity.jpg", 25, 25, true, true)));
+	                e.setGraphic(new ImageView(new Image("file:src/images/data.png", 25, 25, true, true)));
 	            });
 			tabPane.widthProperty().addListener((observable, oldValue, newValue) -> {
 				tabPane.setTabMinWidth(tabPane.getWidth() / tabPane.getTabs().size());
