fa5a417d6330b285cc6ec2f303a3ad69a0920e7b
Mert Sahin <mert.sahin@student.hogent.be>
2022-03-05 18:02:54 +0100

    borderpane lost fullscreen oplossing op

--- src/gui/TestFrameController.java
+++ src/gui/TestFrameController.java
@@ -1,7 +1,33 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 
-public class TestFrameController {
-
+public class TestFrameController extends BorderPane {
+	@FXML
+	private TabPane tabPane;
+	
+	public TestFrameController() {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("TestFrame.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+		try
+		{
+			loader.load();
+			
+			tabPane.widthProperty().addListener((observable, oldValue, newValue) -> {
+				tabPane.setTabMinWidth(tabPane.getWidth() / tabPane.getTabs().size());
+				tabPane.setTabMaxWidth(tabPane.getWidth() / tabPane.getTabs().size());
+			});
+			
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
 }
---
+++ src/main/TestGUI.java
@@ -0,0 +1,42 @@
+package main;
+
+import domein.AanmeldController;
+import gui.AanmeldSchermController;
+import gui.TestFrameController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import repository.DatabaseSelector;
+
+public class TestGUI extends  Application {
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+
+			TestFrameController root = new TestFrameController();
+			Scene scene = new Scene(root);
+			primaryStage.setResizable(true);
+			primaryStage.setMaximized(true);
+			primaryStage.setScene(scene);
+			
+			scene.getStylesheets().add("theme.css");
+			
+			Image icon = new Image(getClass().getResource("/images/icon.png").toExternalForm());
+			primaryStage.getIcons().add(icon);
+			
+			primaryStage.show();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
