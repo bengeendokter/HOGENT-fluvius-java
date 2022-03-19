e8f0f12f8058ca11b17f0481137eb3c476f3b5e9
An-Sofie Vannieuwenhuyze <ansofie.vannieuwenhuyze@student.hogent.be>
2022-03-19 11:34:37 +0100

    disable root component treeview

--- src/gui/CategorieDetailPanel.java
+++ src/gui/CategorieDetailPanel.java
@@ -107,7 +107,7 @@ public class CategorieDetailPanel<E> extends GridPane{
  
  
         TreeView<SdGoal> treeView = new TreeView<SdGoal>(rootNode);
-        
+        treeView.setShowRoot(false);
         this.add(treeView, 0,10);
 		
 		
