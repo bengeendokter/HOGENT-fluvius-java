c209342cba2b2efe3295c789f1b66ec9975d58d6
Mert Sahin <mert.sahin@student.hogent.be>
2022-03-22 11:44:03 +0100

    meta-data bij datasource

--- src/domein/Datasource.java
+++ src/domein/Datasource.java
@@ -29,10 +29,12 @@ public interface Datasource{
 	//meta data toevoegen als attribuut
 	//file kunnen uploaden
 	//wijzigingen worden opgeslagen
+	//true bij corrupt
 	public boolean getCorrupt();
 	public LocalDate getDatum();
 	public String getMaat();
 	public String getWijzigbaarheid();
+	//true als het moet wijzigen
 	public boolean wijzigNood();
 	public int getKolom();
 	
