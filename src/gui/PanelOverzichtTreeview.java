85d381b032d29e49b16005089529774c85602097
Ben <79012763+benartshogent@users.noreply.github.com>
2022-03-25 09:23:27 +0100

    Doelstellingen Treeview

--- src/gui/PanelOverzichtTreeview.java
+++ src/gui/PanelOverzichtTreeview.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.util.List;
 import domein.Doelstelling;
 import domein.DomeinController;
 import javafx.application.Platform;
@@ -19,68 +20,39 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
-public class PanelOverzichtTreeview extends VBox {
-	
-//	private ObservableList<Doelstelling> items;
-//	private DomeinController dc;
+public class PanelOverzichtTreeview extends VBox
+{
 	TreeView<Doelstelling> treeview = new TreeView<>();
-
-	public PanelOverzichtTreeview() {
+	
+	public PanelOverzichtTreeview()
+	{
 		
-		//initGui(items, "doelstellingen", dc);
 	}
-
-	public void initGui(ObservableList<Doelstelling> items, String soort, DomeinController dc) {
-//		this.items = items;
-//		this.dc = dc;
-		
-		this.getChildren().clear();
-		
+	
+	public void initGui(ObservableList<Doelstelling> doelstellingen, String soort, DomeinController dc)
+	{		
 		this.setStyle("-fx-background-color: #495973;");
 		
 		// Label aanmaken
 		Label lblOverzicht = new Label("Overzicht " + soort);
 		lblOverzicht.setStyle("-fx-text-fill: white;  -fx-font-size: 20;-fx-font-weight: bold; ");
 		
+		// maak rootNode
+		TreeItem<Doelstelling> rootNode = new TreeItem<Doelstelling>(null);
 		
+		// vul rootNode recursief op met doelstellingen
+		List<Doelstelling> rootDoelstellingen = doelstellingen.stream()
+				.filter(doel -> doel.getParentComponent() == null).toList();
+		addToTreeItem(rootNode, rootDoelstellingen);
 		
-		TreeItem<Doelstelling> rootNode = 
-		        new TreeItem<Doelstelling>(null);
-
-		for (Doelstelling s : items) {
-			System.out.println(s.getNaam());
-            TreeItem<Doelstelling> empLeaf = new TreeItem<Doelstelling>(s);
-            boolean found = false;
-            for (TreeItem<Doelstelling> depNode : rootNode.getChildren()) {
-            	if(s.getParentComponent() != null) {
-            		if(depNode.getValue().getDoelstellingID() == s.getParentComponent().getDoelstellingID()) {
-                		depNode.getChildren().add(empLeaf);
-                      found = true;
-                      break;
-                	}
-            	}	
-            }
-            String pad = s.getIcon();
-			int index = pad.indexOf("c");
-			pad = pad.substring(index+1);
-
-            if (!found) {
-                TreeItem<Doelstelling> depNode = new TreeItem<Doelstelling>(
-                    s, new ImageView(new Image(s.getIcon(), 30, 30, true, true))
-                );
-                
-                rootNode.getChildren().add(depNode);
-            }
-        }
- 
 		treeview.setRoot(rootNode);
 		treeview.setShowRoot(false);
 		
-
-		
+		// stel iconen en namen van TreeCellen in
 		treeview.setCellFactory(param -> new TreeCell<Doelstelling>()
 		{
 			private ImageView imageView = new ImageView();
+			
 			@Override
 			public void updateItem(Doelstelling doel, boolean empty)
 			{
@@ -96,17 +68,12 @@ public class PanelOverzichtTreeview extends VBox {
 					imageView.setImage(new Image(doel.getIcon(), 30, 30, true, true));
 					
 					setGraphic(imageView);
-
+					
 				}
 			}
 		});
 		
-		// Eventhandler vervangen met Listenen zodat eerste doelstelling direct kan worden weergegeven
-//		EventHandler<MouseEvent> mouseEventHandle = (MouseEvent event) -> {
-//		    handleMouseClicked(event);
-//		};
-//
-//		treeview.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEventHandle);
+		// on click
 		treeview.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
 			if(newValue != null)
 			{
@@ -116,16 +83,16 @@ public class PanelOverzichtTreeview extends VBox {
 				if(hoofdScherm instanceof BorderPane)
 				{
 					DoelstellingDetailsTest test = new DoelstellingDetailsTest(dc, newValue.getValue());
-
-						Platform.runLater(new Runnable()
+					
+					Platform.runLater(new Runnable()
+					{
+						@Override
+						public void run()
 						{
-							@Override
-							public void run()
-							{
-								((BorderPane) hoofdScherm).setCenter(test);
-							}
-						});
-
+							((BorderPane) hoofdScherm).setCenter(test);
+						}
+					});
+					
 				}
 			}
 		});
@@ -133,28 +100,30 @@ public class PanelOverzichtTreeview extends VBox {
 		// Butten aanmaken #B2D234
 		Button btnMaakNieuwe = new Button("+");
 		btnMaakNieuwe.setStyle("-fx-background-color: #B2D234;-fx-text-fill: white;  -fx-font-size: 20;");
-
-		btnMaakNieuwe.setOnAction(new EventHandler<ActionEvent>() {
+		
+		btnMaakNieuwe.setOnAction(new EventHandler<ActionEvent>()
+		{
 			@Override
-			public void handle(ActionEvent evt) {
-
-				if(soort.equals("doelstellingen")) {
-					UpdateOrCreateDoelstelling<Doelstelling> vs = new UpdateOrCreateDoelstelling<>(dc, null, "Maak nieuwe doelstelling");
+			public void handle(ActionEvent evt)
+			{
+				if(soort.equals("doelstellingen"))
+				{
+					UpdateOrCreateDoelstelling<Doelstelling> vs = new UpdateOrCreateDoelstelling<>(dc, null,
+							"Maak nieuwe doelstelling");
 					// Eerst het hoofdscherm opvragen adhv dit scherm
 					Parent hoofdScherm = PanelOverzichtTreeview.this.getParent();
-					if (hoofdScherm instanceof BorderPane) {
+					if(hoofdScherm instanceof BorderPane)
+					{
 						// DetailsScherm opvragen adhv het hoofdScherm
 						((BorderPane) hoofdScherm).setCenter(vs);
 					}
 				}
-				
-				
 			}
 		});
 		
 		// Toevoegen aan het scherm
 		this.getChildren().addAll(lblOverzicht, treeview, btnMaakNieuwe);
-
+		
 		// Ruimte instellen
 		this.setPadding(new Insets(10));
 		this.setSpacing(5);
@@ -165,31 +134,16 @@ public class PanelOverzichtTreeview extends VBox {
 		treeview.getSelectionModel().selectFirst();
 	}
 	
-//	private void handleMouseClicked(MouseEvent event) {
-//	    Node node = event.getPickResult().getIntersectedNode();
-//	    if (node instanceof Text || (node instanceof TreeCell && ((TreeCell) node).getText() != null)) {
-//	    	TreeItem c = (TreeItem)treeview.getSelectionModel().getSelectedItem();
-//	    	
-//	    	
-//	    	Parent hoofdScherm = PanelOverzichtTreeview.this.getParent();
-//			
-//			if (hoofdScherm instanceof BorderPane) {
-//
-//				 //DetailsScherm opvragen adhv het hoofdScherm
-//				Node details = ( ((BorderPane) hoofdScherm).getCenter());
-//
-//					//DoelstellingDetailPanel dd = new DoelstellingDetailPanel(dc, newValue);
-//					DoelstellingDetailsTest test = new DoelstellingDetailsTest(dc, c.getValue());						//DoelstellingDetailPanelController2 d2 = new DoelstellingDetailPanelController2(dc, newValue);
-//						((BorderPane) hoofdScherm).setCenter(test);
-//
-//	    	
-//	    	
-//	    	
-//          
-//	    	
-//	    }
-//	} 
-//	}
-
-
+	private void addToTreeItem(TreeItem<Doelstelling> rootDoelstelling, List<Doelstelling> doelstellingen)
+	{
+		for(Doelstelling doelstelling : doelstellingen)
+		{
+			TreeItem<Doelstelling> parentDoelstelling = new TreeItem<>(doelstelling);
+			
+			addToTreeItem(parentDoelstelling,
+					doelstelling.getComponents().stream().map(component -> (Doelstelling) component).toList());
+			
+			rootDoelstelling.getChildren().add(parentDoelstelling);
+		}
+	}
 }
